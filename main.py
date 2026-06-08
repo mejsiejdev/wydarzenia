40c3ab8c1455b8690534064499a8c1f9f51015f6
Jakub Brzostowski <youngkuba05@gmail.com>
2026-06-08 23:56:43 +0200

    feature: dodano mozliwosc tworzenia, listowania i getowania eventow

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
 from config import settings
-from routers import users
+from routers import users, events
 
 app = FastAPI(title=settings.app_name)
 
 app.include_router(users.router)
+app.include_router(events.router)
--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime
-
+from enum import Enum
 from pydantic import BaseModel, EmailStr, Field
 
 
@@ -28,3 +28,32 @@ class UserUpdate(BaseModel):
     password: str | None = Field(default=None, min_length=8, max_length=72)
     first_name: str | None = Field(default=None, min_length=1)
     last_name: str | None = Field(default=None, min_length=1)
+
+
+class EventStatus(str, Enum):
+    szkic = "szkic"
+    oczukujace_na_akceptacje = "oczukujace_na_akceptacje"
+    zaakceptowane = "zaakceptowane"
+    odrzucone = "odrzucone"
+
+
+class EventCreate(BaseModel):
+    title: str = Field(min_length=1)
+    description: str
+    public: bool
+    status: EventStatus = EventStatus.szkic
+    participant_limit: int
+    social_media_links: str
+    created_by: uuid.UUID
+
+
+class EventRead(BaseModel):
+    id: uuid.UUID
+    title: str
+    description: str
+    public: bool
+    status: EventStatus
+    participant_limit: int
+    social_media_links: str
+    created_at: datetime
+    created_by: uuid.UUID
