91f7d1043093cf954386476dd9e0482a4e4ecf54
Jakub Brzostowski <youngkuba05@gmail.com>
2026-06-14 13:14:50 +0200

    fix heads

--- alembic/versions/359ac55e8401_add_fk_event_registrations.py
+++ alembic/versions/359ac55e8401_add_fk_event_registrations.py
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "359ac55e8401"
-down_revision: Union[str, Sequence[str], None] = "dcd0733ad720"
+down_revision: Union[str, Sequence[str], None] = "5fcdc38d0b52"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
